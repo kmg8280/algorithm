b81727237c25f87744769f54538bda8f05e079a4
MinGW <kmg8280@gmail.com>
2019-11-21 02:53:46 +0900

    9250

--- PS/9250.cpp
+++ PS/9250.cpp
@@ -23,7 +23,7 @@ void makeFailure()
 {
 	queue<P> q;
 	q.push({ -1,1 });
-	fLink[1] = -1;
+	fill(fLink, fLink + 1000100, -1);
 	while (!q.empty())
 	{
 		int ti = q.front().first;
@@ -31,22 +31,20 @@ void makeFailure()
 		q.pop();
 		for (auto x : trie[tj])
 		{ // x 형태는 key value로 pair 형태로 반환
-			// x.second 가 다음노드
-			// x.first가 간선 문자
 			int i = ti;
 			int j = tj;
 
-			while (i > 0 && !trie[i].count(x.first) && exist[i])
+			while (i > 0 && !trie[i].count(x.first))
 				i = fLink[i]; // 경로가 없으면 string이 겹치는 노드로 가야함
-			if (i == -1)
+			if (i < 0)
 				i = 1; // -1이면 루트
 			else
 				i = trie[i][x.first]; // -1이 아니면 특정 노드에 있고 다음단계가 있다는 뜻이니 이동
 
 			j = x.second;
 			fLink[j] = i;
-			if (exist[i]) // exist가 있다면 최대 suffix부분이 있다는 뜻
-				exist[j] = true; // 
+			if (exist[i])
+				exist[j] = true; 
 			q.push({ i,j });
 		}
 	}
@@ -75,7 +73,7 @@ int main()
 		mark[cur] = true;
 		exist[cur] = true;
 	}
-
+	makeFailure();
 	cin >> q;
 	while (q--)
 	{
@@ -85,14 +83,16 @@ int main()
 		bool check = false;
 		for (int i = 0; i < size; i++)
 		{
-			while (cur > 0 && !trie[cur].count(input[i]) && exist[cur])
+			while (cur > 0 && !trie[cur].count(input[i]))
 				cur = fLink[cur]; // 루트가 아닌데 경로가 없으면 이동
 
-			//cout << cur << '\n';
+			if (cur == -1)
+				cur = 1;
+
 			if (trie[cur].count(input[i]))
 				cur = trie[cur][input[i]];
 
-			if (mark[cur])
+			if (exist[cur])
 			{
 				check = true;
 				break;
